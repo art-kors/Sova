7bdae562b4ba394269fa461c0b1a7e0c83456b1a
art-kors <94845623+art-kors@users.noreply.github.com>
2022-03-20 17:05:50 +0300

    Update Sova_dark_v.py

--- Sova_dark_v.py
+++ Sova_dark_v.py
@@ -298,3 +298,20 @@ async def helper(ctx):
 
 
 bot.run(TOKEN)
+
+
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Ready"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
